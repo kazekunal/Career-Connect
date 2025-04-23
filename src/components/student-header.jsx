70e92617ef57826e2125279ded9eeb1528922e2f
ChirayuXD <chirayuagrawal321@gmail.com>
2025-04-23 23:40:59 +0530

    noti changes

--- src/components/student-header.jsx
+++ src/components/student-header.jsx
@@ -118,10 +118,10 @@ export function StudentHeader() {
             </Sheet>
           </div>
 
-          <div className="hidden md:flex items-center gap-4">
-            <Link href="/student/notifications" className="hover:text-blue-200">
+          {/* <div className="hidden md:flex items-center gap-4">
+            <Link href="/notifications" className="hover:text-blue-200">
               <Bell className="h-5 w-5" />
-            </Link>
+            </Link> */}
 
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
